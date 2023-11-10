2b7db2d26cc0edd30674fc4f1998c902e0b481e5
Tyrone Williams <Tyronemwilliams@outlook.com>
2023-11-10 20:53:23 +0000

    Feat: add relative time to logger

--- lib/key_logger.py
+++ lib/key_logger.py
@@ -1,23 +1,30 @@
 from pynput import mouse, keyboard
+from time import time
 #pynput             1.7.6
 
 # Declare mouse listener globally, so that keyboard on_release can stop it
 
 mouse_listener = None
+# declare our start_time globally so that the call back functions can reference it.
+start_time = None
 
 def main():
     runListeners()
 
+def elapsed_time():
+    global start_time
+    return time() - start_time
+
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('{0} pressed at {1}'.format(
+            key.char, elapsed_time()))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed at {1}'.format(
+            key, elapsed_time()))
         
 def on_release(key):
-    print('{0} released'.format(key))
+    print('{0} released'.format(key, elapsed_time()))
     
     if key == keyboard.Key.esc:
         # Stop listener
@@ -29,7 +36,7 @@ def on_release(key):
     
 def on_click(x, y, button, pressed):
     if not pressed:
-        print('{0} at {1}'.format('Released',(x, y)))
+        print('clicked {0} on {1} at {2}'.format(button,(x, y), elapsed_time()))
  
 def runListeners():
 
@@ -43,6 +50,8 @@ def runListeners():
     with keyboard.Listener(
             on_press=on_press,
             on_release=on_release) as listener:
+        global start_time
+        start_time = time()
         listener.join()
     # collect keyboard input
 
