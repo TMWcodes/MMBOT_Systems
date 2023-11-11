16710c9c55e20a1800c526b9168a5b679d7ce080
Tyrone Williams <Tyronemwilliams@outlook.com>
2023-11-11 12:39:03 +0000

    Feat: add perform actions from json file

--- lib/playback.py
+++ lib/playback.py
@@ -30,8 +30,36 @@ def playActions(filename):
     )
     with open(file_path, "r") as jsonfile:
         #formatting
+        # parse json first
         data = json.load(jsonfile)
-        print(data)
+        for index, action in enumerate(data):
+            # look for esc
+            if action['button'] == 'Key.esc':
+                break
+
+            #perform action
+            if action['type'] == 'keyDown':
+                pyautogui.keyDown(action['button'])
+                print("keyDown on {}". format(action['button']))
+            elif action['type'] == 'keyUp':
+                pyautogui.keyUp(action['button'])
+                print("keyUp on {}". format(action['button']))
+            elif action['type'] == 'click':
+                pyautogui.click(action['pos'][0],action['pos'][1], duration=0.25)
+                print("click on {}". format(action['pos']))
+
+            # sleep until next action
+            try:
+                next_action = data[index + 1]
+            except IndexError:
+                break
+            elapsed_time = next_action['time'] -action['time']
+            if elapsed_time >= 0:
+                print("sleeping for {}".format(elapsed_time))
+                sleep(elapsed_time)
+            else:
+                raise Exception('Unexpected action ordering')
+        # print(data)
 
 if __name__ == "__main__":
     main()
