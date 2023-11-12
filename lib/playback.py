cfb7b64ba37a7f8fba98c29a5512d89cca8c8712
Tyrone Williams <Tyronemwilliams@outlook.com>
2023-11-12 12:22:41 +0000

    Fix: add space, enter and backspace to playback

--- lib/playback.py
+++ lib/playback.py
@@ -1,5 +1,5 @@
 import pyautogui
-from time import sleep
+from time import sleep, time
 import os #import
 import json #parse
 def main():
@@ -32,17 +32,21 @@ def playActions(filename):
         #formatting
         # parse json first
         data = json.load(jsonfile)
+        
         for index, action in enumerate(data):
             # look for esc
+            action_start_time = time()
             if action['button'] == 'Key.esc':
                 break
 
             #perform action
             if action['type'] == 'keyDown':
-                pyautogui.keyDown(action['button'])
+                key = convertKey(action['button'])
+                pyautogui.keyDown(key)
                 print("keyDown on {}". format(action['button']))
             elif action['type'] == 'keyUp':
-                pyautogui.keyUp(action['button'])
+                key = convertKey(action['button'])
+                pyautogui.keyUp(key)
                 print("keyUp on {}". format(action['button']))
             elif action['type'] == 'click':
                 pyautogui.click(action['pos'][0],action['pos'][1], duration=0.25)
@@ -53,13 +57,45 @@ def playActions(filename):
                 next_action = data[index + 1]
             except IndexError:
                 break
-            elapsed_time = next_action['time'] -action['time']
-            if elapsed_time >= 0:
-                print("sleeping for {}".format(elapsed_time))
-                sleep(elapsed_time)
-            else:
+            elapsed_time = next_action['time'] - action['time']
+
+            if elapsed_time < 0:
                 raise Exception('Unexpected action ordering')
-        # print(data)
+
+            elapsed_time -= (time() - action_start_time)
+            if elapsed_time < 0:
+                elapsed_time = 0
+            print("sleeping for {}".format(elapsed_time))
+            sleep(elapsed_time)
+
+def convertKey(button):
+    # 'Key.F9' should return 'F9', 'w' should return 'w'
+    PYNPUT_SPECIAL_CASE_MAP = {
+        'alt_l': 'altleft',
+        'alt_r': 'altright',
+        'alt_gr': 'altright',
+        'caps_lock': 'capslock',
+        'ctrl_l': 'ctrlleft',
+        'ctrl_r': 'ctrlright',
+        'page_down': 'pagedown',
+        'page_up': 'pageup',
+        'shift_l': 'shiftleft',
+        'shift_r': 'shiftright',
+        'num_lock': 'numlock',
+        'print_screen': 'printscreen',
+        'scroll_lock': 'scrolllock',
+        'Key.space': ' ',
+        'enter': '\n',
+        'backspace': '\b',
+    }
+
+    # example: 'Key.F9' should return 'F9', 'w' should return as 'w'
+    cleaned_key = button.replace('Key.', '')
+    print(cleaned_key)
+    if cleaned_key in PYNPUT_SPECIAL_CASE_MAP:
+        return PYNPUT_SPECIAL_CASE_MAP[cleaned_key]
+
+    return cleaned_key
 
 if __name__ == "__main__":
     main()
